4bcab1d08989a2f9afb4e533b76dfd48780191c6
tims-crew <72353134+tims-crew@users.noreply.github.com>
2022-01-03 17:39:26 +0500

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,12 +1,36 @@
+/** 1) Install & Set up mongoose */
 const mongoose = require('mongoose');
-const { Schema } = require('mongoose');
+mongoose.connect(process.env.MONGO_URI);
 
-const personSchema = new Schema({
-    name: {type: String, required: true},
-    age:  Number,
-    favoriteFoods: [String]
+/** 2) Create a 'Person' Model */
+var personSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+  favoriteFoods: [String]
 });
 
-const Person = mongoose.model('Person', personSchema);
+/** 3) Create and Save a Person */
+var Person = mongoose.model('Person', personSchema);
 
-const tom = new Person({name: "Thomphson", age: 14, favoriteFoods: ["burgers", "pizza"]});
+var createAndSavePerson = function(done) {
+  var janeFonda = new Person({name: "Jane Fonda", age: 84, favoriteFoods: ["eggs", "fish", "fresh fruit"]});
+
+  janeFonda.save(function(err, data) {
+    if (err) return console.error(err);
+    done(null, data)
+  });
+};
+
+/** 4) Create many People with `Model.create()` */
+var arrayOfPeople = [
+  {name: "Frankie", age: 74, favoriteFoods: ["Del Taco"]},
+  {name: "Sol", age: 76, favoriteFoods: ["roast chicken"]},
+  {name: "Robert", age: 78, favoriteFoods: ["wine"]}
+];
+
+var createManyPeople = function(arrayOfPeople, done) {
+  Person.create(arrayOfPeople, function (err, people) {
+    if (err) return console.log(err);
+    done(null, people);
+  });
+};
